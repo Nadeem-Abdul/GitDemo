2a3e75f380f680a1821b7cf5d6ef6c6402e58e66
nadeem <abdulnadeem314@gmail.com>
2021-06-24 11:29:52 +0530

    This is the first commit

---
+++ src/test/java/nadeemlateef/com/RetsAPITest.java
@@ -0,0 +1,18 @@
+package nadeemlateef.com;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class RetsAPITest {
+	
+	@Test
+	public void PostJira() {
+		//Assert.assertTrue(false);
+		System.out.println("PostJira");
+	}
+	
+	@Test
+	public void deleteTwitter() {
+		System.out.println("deleteTwitter");
+	}
+}
